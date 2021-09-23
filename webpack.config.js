3b21fa8d4fa2f9353efb0993fe248a75e5ee44df
lzn01 <408135242@qq.com>
2021-09-23 19:31:24 +0800

    安装ts 使用webpack打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,18 @@
+const path = require('path')
 // 需要导出的对象
 module.exports = {
+    // 生产模式
+    mode:'production',
     // 程序入口
     entry: {
-        index: './lib/index.tsx'
+        NUI: './lib/index.tsx' // 这里就是文件名
+    },
+    // 代码输出目录
+    output: {
+        // 要写绝对路径 (操作系统原因)
+        path: path.resolve(__dirname, 'dist/lib'),
+        library: 'NUI',
+        libraryTarget: 'umd',
     },
     // 解释.tsx
     module: {
